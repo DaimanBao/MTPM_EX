871163fb8bd1fbd0c434860643370d83ce17bf9f
HoangBao <vohoangbaoqn123@gmail.com>
2026-03-05 14:05:05 +0700

    first commit

---
+++ Design_Pattern/src/ex1/PrintSpooler.java
@@ -0,0 +1,27 @@
+package ex1;
+
+public class PrintSpooler {
+	   private static PrintSpooler instance;
+
+	   private PrintSpooler() {
+	       init();
+	   }
+
+	   private void init() {
+	       System.out.println("Initializing Print Spooler...");
+	   
+	   }
+
+	 
+	   public static PrintSpooler getInstance() {
+	       if (instance == null) {
+	           instance = new PrintSpooler();
+	       }
+	       return instance;
+	   }
+
+
+	   public void print(String document) {
+	       System.out.println("Printing: " + document);
+	   }
+}
---
+++ Design_Pattern/src/ex2/Candy.java
@@ -0,0 +1,6 @@
+package ex2;
+
+public abstract class Candy {
+    public abstract void prepare();
+    public abstract double getPrice();
+}
